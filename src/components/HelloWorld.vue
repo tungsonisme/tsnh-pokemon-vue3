d01bba6f665b33049235df4482a2457a18c098cc
Sean <tungson.nguyen@shopee.com>
2022-12-29 01:03:47 +0800

    initialize project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import federation from '@originjs/vite-plugin-federation';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.vue',
+      },
+      shared: ['vue'],
+    }),
+  ],
+});
